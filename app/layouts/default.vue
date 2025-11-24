3d80b642ad2bcab6c2b6049ec4b0043032e5179d
Pedro <2022302914@aluno.erechim.ifrs.edu.br>
2025-11-24 19:57:38 -0300

    refactor: fixing favorites list

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,9 +3,9 @@
     <AppHeader/>
 
     <div class="flex flex-row w-1/1">
-      <div class="bg-sky-200 w-1/5">
-        <template v-for="i in 10">
-          <p class="w- bg-sky-600 rounded-[2vw] m-3">Favorito {{ i }}</p>
+      <div class="bg-sky-200 w-1/5 p-5">
+        <template v-for="i in 15">
+          <p class="w- bg-sky-600 rounded-[2vw] m-3 p-3">Favorito {{ i }}</p>
         </template>
       </div>
       <div class="bg-sky-200 w-4/5" >
